378f5973917aff361426fce6aaf288caf0ce1264
fraanciiss04 <francisco.puras.alu@santamaria-artesano.org>
2023-02-17 10:01:20 +0100

    versión BETA

---
+++ src/main/java/com/mycompany/cvspuras/Clase2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cvspuras;
+
+/**
+ *
+ * @author Francisco Puras
+ */
+public class Clase2 {
+    public static void main(String[] args) {
+        System.out.println("Entornos");
+        System.out.println("Estamos en la rama Producción");
+    }
+}
